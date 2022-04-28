b8f162f153441193712de3b3fcc0c025034adfca
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-04-28 20:43:06 +0200

    feat(search): added dropdown to facets gategories

--- src/components/query/Facets.js
+++ src/components/query/Facets.js
@@ -1,16 +1,72 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchInitialFacets, searchReferences, setSearchFacetsValues} from '../../actions/queryActions';
 import Form from 'react-bootstrap/Form';
-import {Badge} from 'react-bootstrap';
+import {Accordion, Badge, Button} from 'react-bootstrap';
+import {IoIosArrowDroprightCircle, IoIosArrowDropdownCircle} from 'react-icons/io';
 
-const Facets = () => {
+const Facet = ({facetsToInclude}) => {
 
     const searchFacets = useSelector(state => state.query.searchFacets);
     const searchQuery = useSelector(state => state.query.searchQuery);
     const searchFacetsValues = useSelector(state => state.query.searchFacetsValues);
     const searchFacetsLimits = useSelector(state => state.query.searchFacetsLimits);
     const dispatch = useDispatch();
+
+    return (
+        <div>
+            {Object.entries(searchFacets).filter(([key, value]) =>
+                facetsToInclude.includes(key.replace('.keyword', '').replace('_', ' ')))
+                .map(([key, value]) =>
+                    <div style={{textAlign: "left", paddingLeft: "2em"}}>
+                        {Object.entries(searchFacets).map(([key, value]) =>
+                            <div>
+                                <h5>{key.replace('.keyword', '').replace('_', ' ')}</h5>
+                                {value.buckets.map(bucket => <div style={{paddingLeft: "1em"}}>
+                                    <Form.Check inline type="checkbox"
+                                                checked={searchFacetsValues.hasOwnProperty(key) && searchFacetsValues[key].includes(bucket.key)}
+                                                onChange={(evt) => {
+                                                    let newSearchFacetsValues = searchFacetsValues;
+                                                    if (evt.target.checked) {
+                                                        if (!newSearchFacetsValues.hasOwnProperty(key)) {
+                                                            newSearchFacetsValues[key] = []
+                                                        }
+                                                        newSearchFacetsValues[key].push(bucket.key)
+                                                    } else {
+                                                        newSearchFacetsValues[key] = newSearchFacetsValues[key].filter(
+                                                            e => e !== bucket.key)
+                                                        if (newSearchFacetsValues[key].length === 0) {
+                                                            delete newSearchFacetsValues[key];
+                                                        }
+                                                    }
+                                                    dispatch(searchReferences(searchQuery, newSearchFacetsValues, searchFacetsLimits));
+                                                }}/>
+                                    {bucket.key} <Badge variant="secondary">{bucket.doc_count}</Badge>
+                                </div>)}
+                            </div>
+                        )}
+                    </div>
+                )}
+        </div>
+    )
+}
+
+
+const Facets = () => {
+
+    const [openFacets, setOpenFacets] = useState(new Set());
+    const searchFacets = useSelector(state => state.query.searchFacets);
+    const dispatch = useDispatch();
+
+    const toggleFacetGroup = (facetGroupLabel) => {
+        let newOpenFacets = new Set([...openFacets]);
+        if (newOpenFacets.has(facetGroupLabel)) {
+            newOpenFacets.delete(facetGroupLabel);
+        } else {
+            newOpenFacets.add(facetGroupLabel);
+        }
+        setOpenFacets(newOpenFacets);
+    }
 
     useEffect(() => {
         if (Object.keys(searchFacets).length === 0) {
@@ -19,34 +75,19 @@ const Facets = () => {
     }, [])
 
     return (
-        <div style={{textAlign: "left", paddingLeft: "1em"}}>
-            {Object.entries(searchFacets).map(([key, value]) =>
-                <div>
-                    <h5>{key.replace('.keyword', '').replace('_', ' ')}</h5>
-                    {value.buckets.map(bucket => <div style={{paddingLeft: "1em"}}>
-                        <Form.Check inline type="checkbox"
-                                    checked={searchFacetsValues.hasOwnProperty(key) && searchFacetsValues[key].includes(bucket.key)}
-                                    onChange={(evt) => {
-                                        let newSearchFacetsValues = searchFacetsValues;
-                                        if (evt.target.checked) {
-                                            if (!newSearchFacetsValues.hasOwnProperty(key)) {
-                                                newSearchFacetsValues[key] = []
-                                            }
-                                            newSearchFacetsValues[key].push(bucket.key)
-                                        } else {
-                                            newSearchFacetsValues[key] = newSearchFacetsValues[key].filter(
-                                                e => e !== bucket.key)
-                                            if (newSearchFacetsValues[key].length === 0) {
-                                                delete newSearchFacetsValues[key];
-                                            }
-                                        }
-                                        dispatch(searchReferences(searchQuery, newSearchFacetsValues, searchFacetsLimits));
-                                    }}/>
-                        {bucket.key} <Badge variant="secondary">{bucket.doc_count}</Badge>
-                    </div>)}
-                </div>
-            )}
-        </div>
+        <Accordion style={{textAlign: "left"}}>
+            <div>
+                <Accordion.Toggle as={Button} variant="light" size="lg" eventKey="0"
+                                  onClick={() => toggleFacetGroup('Bibliographic Data')}>
+                    {openFacets.has('Bibliographic Data') ? <IoIosArrowDropdownCircle/> : <IoIosArrowDroprightCircle/>} Bibliographic Data
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <div>
+                        <Facet facetsToInclude={["pubmed type"]}/>
+                    </div>
+                </Accordion.Collapse>
+            </div>
+        </Accordion>
     )
 }
 
