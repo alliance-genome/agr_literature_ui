6333dfe9753ed4446c7f8f567ab8ad85d332a71e
Juancarlos <azurebrd@its.caltech.edu>
2022-05-25 21:09:03 +0000

    SCRUM-1586 removed stuff from navbar SCRUM-1579 change navbar from query to Search

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,26 +10,18 @@ import {oktaSignInConfig} from "../config";
 
 const devOrProd = process.env.REACT_APP_DEV_OR_PROD;
 var navClass = (devOrProd === 'dev') ? "Navbar-dev" : "Navbar";
-var homeLabel = (devOrProd === 'dev') ? "Dev" : "Prod";
+var homeLabel = (devOrProd === 'dev') ? "Stage" : "Prod";
 
 const NavigationBar = () => {
   return (
   <Navbar className={navClass} >
     <Nav className="justify-content-center"  style={{ flex: 1}}>
-      <Nav.Link as={Link} to="/">{homeLabel}</Nav.Link>
-      <Nav.Link as={Link} to="/query">Query</Nav.Link>
-      <Nav.Link as={Link} to="/biblio">Biblio</Nav.Link>
+      <Nav.Link >{homeLabel}</Nav.Link>
+      <Nav.Link as={Link} to="/query">Search</Nav.Link>
+      <Nav.Link as={Link} to="/biblio">Biblio Edit</Nav.Link>
       <Nav.Link as={Link} to="/sort">Sort</Nav.Link>
-      <Nav.Link as={Link} to="/flags">Flags & Tags</Nav.Link>
-      <Nav.Link as={Link} to="/files">Files</Nav.Link>
-      <NavDropdown title="Mining" id="basic-nav-dropdown">
-        <NavDropdown.Item as={Link} to="/mining">All Mining</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to="/ontomate">Ontomate</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to="/textpresso">Textpresso</NavDropdown.Item>
-      </NavDropdown>
       <Nav.Link as={Link} to="/create">Create</Nav.Link>
       <Nav.Link as={Link} to="/merge">Merge</Nav.Link>
-      <Nav.Link as={Link} to="/about">About</Nav.Link>
       <Nav.Link as={Link} to="/swaggerUI">Swagger</Nav.Link>
     </Nav>
     <Nav>
@@ -38,6 +30,17 @@ const NavigationBar = () => {
   </Navbar>
   )
 }
+
+//       <Nav.Link as={Link} to="/">{homeLabel}</Nav.Link>
+//       <Nav.Link as={Link} to="/flags">Flags & Tags</Nav.Link>
+//       <Nav.Link as={Link} to="/files">Files</Nav.Link>
+//       <NavDropdown title="Mining" id="basic-nav-dropdown">
+//         <NavDropdown.Item as={Link} to="/mining">All Mining</NavDropdown.Item>
+//         <NavDropdown.Item as={Link} to="/ontomate">Ontomate</NavDropdown.Item>
+//         <NavDropdown.Item as={Link} to="/textpresso">Textpresso</NavDropdown.Item>
+//       </NavDropdown>
+//       <Nav.Link as={Link} to="/about">About</Nav.Link>
+
 //<Nav.Link as={Link} to="/login">Login</Nav.Link>
 //      <Nav.Link as={Link} to="/login">Login</Nav.Link>
 //   <Navbar bg="dark" variant="dark" >
