86348b22214e95aa946dff127d8695cec5bef748
Juancarlos <azurebrd@its.caltech.edu>
2022-10-04 16:43:16 +0000

    SCRUM-2049 Okta Help mouseover title text and changed OKTA to Okta in modal text to be consistent.

--- src/components/OktaHelp.js
+++ src/components/OktaHelp.js
@@ -13,13 +13,13 @@ const OktaHelp = () => {
   const handleShow = () => setShow(true);
   return (
     <>
-      <Button  style={{marginInline: '.5rem', alignSelf: 'center'}} variant="light" size="sm" type="button" onClick={handleShow}>Okta Help</Button>
+      <Button style={{marginInline: '.5rem', alignSelf: 'center'}} variant="light" size="sm" type="button" onClick={handleShow} title="Okta is the user authentication system used by the ABC.  Please click here for more information about how to get an Okta account.">Okta Help</Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Okta Account Help</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          For help setting up an OKTA account please contact the OKTA administrator for your MOD or the Team where you are working.<br/><br/>
+          For help setting up an Okta account please contact the Okta administrator for your MOD or the Team where you are working.<br/><br/>
           <Container fluid>
             <Row><Col>Chris Tabone</Col><Col>FB</Col><Col>Specialist</Col></Row>
             <Row><Col>Olin Blodgett</Col><Col>MGI</Col><Col>A-Team</Col></Row>
