ebb436c7318aa5e55addcaf062609ee71394ee52
Juancarlos <azurebrd@its.caltech.edu>
2022-10-18 06:00:35 +0000

    default to blank date range if there's no date range in data from api.

--- src/components/Biblio.js
+++ src/components/Biblio.js
@@ -1703,10 +1703,12 @@ const BiblioDateComponent = ({referenceJsonLive}) => {
   const dispatch = useDispatch();
   console.log(referenceJsonLive);
   const now = new Date();
-  const dateRangeStart = ('date_published_start' in referenceJsonLive && referenceJsonLive['date_published_start'] !== null) ?
-                         referenceJsonLive['date_published_start'] : new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
-  const dateRangeEnd = ('date_published_end' in referenceJsonLive && referenceJsonLive['date_published_end'] !== null) ?
-                       referenceJsonLive['date_published_end'] : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
+//   const dateRangeStart = ('date_published_start' in referenceJsonLive && referenceJsonLive['date_published_start'] !== null) ?
+//                          referenceJsonLive['date_published_start'] : new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
+//   const dateRangeEnd = ('date_published_end' in referenceJsonLive && referenceJsonLive['date_published_end'] !== null) ?
+//                        referenceJsonLive['date_published_end'] : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
+  const dateRangeStart = ('date_published_start' in referenceJsonLive) ? referenceJsonLive['date_published_start'] : null
+  const dateRangeEnd = ('date_published_end' in referenceJsonLive) ? referenceJsonLive['date_published_end'] : null
   const [dateRangeArray, onChangeDateRange] = useState([dateRangeStart, dateRangeEnd]);
   useEffect( () => {
     console.log(dateRangeArray)
