257994e75a1a9afd263c84dd87d7a9cc1b7fbcba
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-10-18 16:54:24 -0700

    feat(datePicker): convert dates from local time zone (used by datePicker) to strings without timezone in the reducer (used by API)

--- src/components/Biblio.js
+++ src/components/Biblio.js
@@ -1701,27 +1701,9 @@ const BiblioSubmitUpdateButton = () => {
 
 const BiblioDateComponent = ({referenceJsonLive}) => {
   const dispatch = useDispatch();
-  console.log(referenceJsonLive);
-  const now = new Date();
-  console.log(referenceJsonLive['date_published_start']);
-  console.log(referenceJsonLive['date_published_end']);
-//   const dateRangeStart = ('date_published_start' in referenceJsonLive && referenceJsonLive['date_published_start'] !== null) ?
-//                          referenceJsonLive['date_published_start'] : new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
-//   const dateRangeEnd = ('date_published_end' in referenceJsonLive && referenceJsonLive['date_published_end'] !== null) ?
-//                        referenceJsonLive['date_published_end'] : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
-  let dateRangeStart = ('date_published_start' in referenceJsonLive) ? referenceJsonLive['date_published_start'] : null
-  if (dateRangeStart !== null) { dateRangeStart = new Date(dateRangeStart + 60000 * dateRangeStart.getTimezoneOffset()) }
-  let dateRangeEnd = ('date_published_end' in referenceJsonLive) ? referenceJsonLive['date_published_end'] : null
-  if (dateRangeEnd !== null) { dateRangeEnd = new Date(dateRangeEnd + 60000 * dateRangeEnd.getTimezoneOffset()) }
-//   const [dateRangeArray, onChangeDateRange] = useState([dateRangeStart, dateRangeEnd]);
-//   useEffect( () => {
-//     console.log(dateRangeArray)
-//     dispatch(changeFieldDatePublishedRange(dateRangeArray));
-//   }, [dateRangeArray]); // eslint-disable-line react-hooks/exhaustive-deps
-//             onChange={(newDateRangeArr) => dispatch(changeFieldDatePublishedRange(newDateRangeArr))}
-//             value={[dateRangeStart, dateRangeEnd]}
-//             onChange={onChangeDateRange}
-//             value={dateRangeArray}
+  const dateRangeStart = ('date_published_start' in referenceJsonLive && referenceJsonLive['date_published_start'] !== null) ? new Date(referenceJsonLive['date_published_start']) : null
+  const dateRangeEnd = ('date_published_end' in referenceJsonLive && referenceJsonLive['date_published_end'] !== null) ? new Date(referenceJsonLive['date_published_end']) : null
+
   return (
           <DateRangePicker
             calendarAriaLabel="Toggle calendar"
@@ -1729,10 +1711,21 @@ const BiblioDateComponent = ({referenceJsonLive}) => {
             dayAriaLabel="Day"
             monthAriaLabel="Month"
             nativeInputAriaLabel="Date"
-            onChange={(newDateRangeArr) => { 
-              newDateRangeArr[0] = new Date(newDateRangeArr[0] - 60000 * newDateRangeArr[0].getTimezoneOffset());
-              newDateRangeArr[1] = new Date(newDateRangeArr[1] - 60000 * newDateRangeArr[1].getTimezoneOffset());
-              dispatch(changeFieldDatePublishedRange(newDateRangeArr))} }
+            onChange={(newDateRangeArr) => {
+              if (newDateRangeArr === null) {
+                dispatch(changeFieldDatePublishedRange([null, null]));
+              } else {
+                if (newDateRangeArr[0] !== null) {
+                  newDateRangeArr[0] = new Date(newDateRangeArr[0].toDateString()).toISOString().substr(
+                      0, 10) + "T00:00:00";
+                }
+                if (newDateRangeArr[1] !== null) {
+                  newDateRangeArr[1] = new Date(newDateRangeArr[1].toDateString()).toISOString().substr(
+                      0, 10) + "T00:00:00";
+                }
+                dispatch(changeFieldDatePublishedRange(newDateRangeArr))
+              }
+            }}
             value={[dateRangeStart, dateRangeEnd]}
             yearAriaLabel="Year"
           />
