bcb7d63bb5912a2d9533948413da4cc07c59b9d3
sweng66 <sweng@stanford.edu>
2023-10-06 22:04:17 -0700

    adding a check. If the mod is SGD, it will display SGD-specific version; otherwise it will display a generic version for other mods

--- src/components/biblio/BiblioEntity.js
+++ src/components/biblio/BiblioEntity.js
@@ -1,12 +1,28 @@
+import {useSelector} from "react-redux";
 import {AlertAteamApiDown} from "../ATeamAlert";
+import TopicEntityCreateSGD from "./topic_entity_tag/TopicEntityCreateSGD";
 import TopicEntityCreate from "./topic_entity_tag/TopicEntityCreate";
 import TopicEntityTable from "./topic_entity_tag/TopicEntityTable";
 
 const BiblioEntity = () => {
+  const oktaMod = useSelector(state => state.isLogged.oktaMod);
+  const testerMod = useSelector(state => state.isLogged.testerMod);
+  const accessLevel = (testerMod !== 'No') ? testerMod : oktaMod;
+  if (accessLevel === 'SGD') {
+    return (
+      <>
+        <AlertAteamApiDown />
+        <TopicEntityCreateSGD key="entityCreate" />
+        <br/>
+        <TopicEntityTable key="entityTable" />
+      </>
+    );
+  };
+    
   return (
       <>
         <AlertAteamApiDown />
-        <TopicEntityCreate key="entityCreate" />
+        <TopicEntityCreateSGD key="entityCreate" />
         <br/>
         <TopicEntityTable key="entityTable" />
       </>
