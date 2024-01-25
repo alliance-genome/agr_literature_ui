51916773edead85455188031f5cf92d2c9a0d280
sweng66 <sweng@stanford.edu>
2024-01-25 15:41:55 -0800

    adding action column to the duplicate tag warning section

--- src/components/biblio/topic_entity_tag/TopicEntityUtils.js
+++ src/components/biblio/topic_entity_tag/TopicEntityUtils.js
@@ -1,3 +1,9 @@
+function handleForceInsertionClick(tag) {
+    // Logic for handling the click event
+    tag['force_inserstion'] = 1;
+    console.log("tag=", JSON.stringify(tag));
+}
+
 export const checkForExistingTags = async (forApiArray, accessToken, accessLevel, dispatch, updateButtonBiblioEntityAdd) => {
     let existingTagResponses = [];
     for (const arrayData of forApiArray.values()) {
@@ -38,21 +44,27 @@ export const checkForExistingTags = async (forApiArray, accessToken, accessLevel
 	}
 	headers.push('note')
 	headers.push('created_by')
-	headers.push('created_by(db)')
-	headers.push('tag_status')
-	
+
+	let fullheaders = [...headers];
+	fullheaders.push('created_by(db)')
+	fullheaders.push('tag_status')
+	fullheaders.push('actions')
+
 	// create table headers
         let tableHTML = "<table class='table table-bordered'><tr>";
-	headers.forEach(header => {
+	fullheaders.forEach(header => {
             tableHTML += `<th>${header}</th>`;
         });
         tableHTML += "</tr>";
+
+	const headerSubset = headers.slice(0, headers.length - 3);
 	
 	// create table rows
         existingTagResponses.forEach(tagResponse => {
 	    const tag = tagResponse.data; 
             tableHTML += "<tr>";
 	    let	creator_in_db = '';
+	    let action_button = null;
             headers.forEach(header => {
                 let value = tag[header];
                 // if there is a corresponding '_name' field, use its value instead
@@ -62,18 +74,20 @@ export const checkForExistingTags = async (forApiArray, accessToken, accessLevel
 		if (header === 'created_by') {
 		    creator_in_db = value;
 		}
-		if (header !== 'created_by(db)' && header !== 'tag_status'){
-                    tableHTML += `<td>${value !== null ? value : ''}</td>`;
-		}
+		tableHTML += `<td>${value !== null ? value : ''}</td>`;
             });
 	    if (tagResponse.status.startsWith("exists:")) {
 		let trimmedStr = tagResponse.status.substring(tagResponse.status.indexOf(':') + 1).trim();
 		let parts = trimmedStr.split(' | ');
 		creator_in_db = parts[0];
 		// updated_by_db = parts[1];
+		action_button = document.createElement("button");
+		action_button.innerHTML = "Force Insertion";
+		action_button.onclick = handleForceInsertionClick(tag);
 	    }
 	    tableHTML += `<td>${creator_in_db}</td>`;
 	    tableHTML += `<td>${tagResponse.message}</td>`;
+	    tableHTML += `<td>${action_button}</td>`;
             tableHTML += "</tr>";
         });
         tableHTML += "</table>";
