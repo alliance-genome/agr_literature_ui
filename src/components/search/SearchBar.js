6c3e11f18cf0c470fb49e6d8d4b933c138c8f88a
sweng66 <sweng@stanford.edu>
2024-02-12 16:28:55 -0800

    adding a '?' image next to 'Search' button to show a popup that displays the search logic when users click on the image

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Dropdown, InputGroup, Spinner} from 'react-bootstrap';
-import {searchReferences, setSearchQuery, setSearchResultsPage, setAuthorFilter,setQueryFields,setPartialMatch} from '../../actions/searchActions';
 import {useDispatch, useSelector} from 'react-redux';
-
+import {
+    searchReferences,
+    setSearchQuery,
+    setSearchResultsPage,
+    setAuthorFilter,
+    setQueryFields
+} from '../../actions/searchActions';
+import question_mark_image from '../../images/question_mark.png';
 
 const SearchBar = () => {
-
     const searchLoading = useSelector(state => state.search.searchLoading);
     const searchQuery = useSelector(state => state.search.searchQuery);
-    const partialMatch = useSelector(state => state.search.partialMatch);
     const fieldToSearch = useSelector(state => state.search.query_fields);
 
     const dispatch = useDispatch();
 
+    const [showHelp, setShowHelp] = useState(false);
+    const helpBtnRef = useRef(null);
+
+    const toggleHelpPopup = () => {
+        setShowHelp(!showHelp);
+    };
+    
     function updateSearchField(text){
       dispatch(setQueryFields(text));
     }
 
     return (
         <>
-            <div style={{width: "40em", margin: "auto"}}>
+            <div style={{width: "40em", margin: "auto", position: "relative"}}>
                 <InputGroup className="mb-2">
                     <Dropdown>
                         <Dropdown.Toggle variant="success" id="dropdown-basic" className="inputGroupLeft">
@@ -53,8 +64,34 @@ const SearchBar = () => {
                               dispatch(setAuthorFilter(''));
                               dispatch(searchReferences());
                             }}>
-                        {searchLoading ? <Spinner animation="border" size="sm"/> : <span>Search</span>  }
+                        {searchLoading ? (
+			  <Spinner animation="border" size="sm" />
+                        ) : (
+                          <>
+                            <span>Search</span>
+                          </>
+                        )}
                     </Button>
+		    <Button ref={helpBtnRef} variant="light" onClick={toggleHelpPopup} style={{ marginLeft: '5px', padding: '0', background: 'none', border: 'none' }}>
+                        <img src={question_mark_image} alt="Help" style={{ width: '20px', height: '20px' }}/>
+                    </Button>
+		    {showHelp && (
+                        <div style={{
+                            position: 'absolute',
+		            top: helpBtnRef.current.offsetTop + helpBtnRef.current.offsetHeight + 5 + 'px', // position right below the "?"
+                            left: helpBtnRef.current.offsetLeft - (300 - helpBtnRef.current.offsetWidth) / 2 + 'px', // center align the popup with the "?"
+                            width: '300px',
+                            padding: '10px',
+                            fontSize: '0.8rem', // smaller text size
+                            backgroundColor: '#EBF5FB', // light blue background
+		            boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+		            textAlign: 'left', // left-aligns the text within the popup
+                            zIndex: 1000,
+                        }}>
+			    The search feature is enabled with wildcard functionality. For instance, querying 'act' would return documents containing any words starting with 'act' (e.g., actin, activities). Additionally, it supports boolean logic searches using the "AND" and "OR" operators. For example, a search for 'actin AND binding' will only retrieve documents that include both "actin" and "binding". Conversely, a search for 'actin OR binding' will return documents containing either "actin", "binding", or both.   
+                        </div>
+                    )}
+		    
                 </InputGroup>
             </div>
         </>
