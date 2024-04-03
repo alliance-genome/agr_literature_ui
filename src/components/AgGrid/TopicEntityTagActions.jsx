4de1e097a23af6c6ff3116d11441030ae313e8d3
Pinglei Zhou <zhou@morgan.harvard.edu>
2024-04-03 18:00:48 +0000

    fix bug for format true/false value

--- src/components/AgGrid/TopicEntityTagActions.jsx
+++ src/components/AgGrid/TopicEntityTagActions.jsx
@@ -33,8 +33,8 @@ export default (props) => {
         let topic=props.data.topic_name;
         let entityType=props.data.entity_type_name;
         let entity=props.data.entity_name;
-        let noData='undefined' ? "null" : props.data.negated.toString();
-        let novelData='undefined' ? "null" : props.data.novel_topic_data.toString();
+        let noData=(props.data.negated === null) ? "null" : props.data.negated.toString();
+        let novelData=(props.data.novel_topic_data  === null) ? "null" : props.data.novel_topic_data.toString();
         setTopicBody(topic);
         setEntityTypeBody(entityType);
         setEntityBody(entity);
