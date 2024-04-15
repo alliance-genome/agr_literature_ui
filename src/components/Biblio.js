f484b092f81e2be1518383cb5f74e16010ef4cda
sweng66 <sweng@stanford.edu>
2024-04-15 14:54:20 -0700

    adding a section for 'Resources for curation'

--- src/components/Biblio.js
+++ src/components/Biblio.js
@@ -15,6 +15,7 @@ import NoAccessAlert from './biblio/NoAccessAlert';
 
 import { RowDisplayString } from './biblio/BiblioDisplay';
 import { reffileCompareFn } from './biblio/BiblioFileManagement';
+import { RowDisplayResourcesForCuration } from './BiblioRowDisplayUtils';
 
 import {
   downloadReferencefile,
@@ -247,6 +248,7 @@ const BiblioTagging = () => {
   rowOrderedElements.push(<RowDisplayString key="title" fieldName="title" referenceJsonLive={referenceJsonLive} referenceJsonDb={referenceJsonDb} />);
   // rowOrderedElements.push(<RowDisplayPmcidCrossReference key="RowDisplayPmcidCrossReference" fieldName="cross_references" referenceJsonLive={referenceJsonLive} referenceJsonDb={referenceJsonDb} />);	// curators no longer want this link
   rowOrderedElements.push(<RowDisplayReferencefiles key="referencefile" fieldName="referencefiles" referenceJsonLive={referenceJsonLive} displayOrEditor="display" />);
+  rowOrderedElements.push(<RowDisplayResourcesForCuration referenceJsonLive={referenceJsonLive} />);
   rowOrderedElements.push(<RowDisplayString key="abstract" fieldName="abstract" referenceJsonLive={referenceJsonLive} referenceJsonDb={referenceJsonDb} />);
   // rowOrderedElements.push(<EntityCreate key="geneAutocomplete"/>);
   return (<><Container>{rowOrderedElements}</Container>
