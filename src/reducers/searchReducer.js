e155a2f9a09eb5900258d274a470777483e63dae
sweng66 <sweng@stanford.edu>
2024-06-14 21:16:31 -0700

    The searchReducer has been updated to handle setting and removing date filters for adding dates to the breadcrumb component.

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -11,7 +11,10 @@ import {
   SEARCH_SET_DATE_PUBLISHED, SEARCH_SET_SEARCH_QUERY_FIELDS,
   SEARCH_SET_SORT_BY_PUBLISHED_DATE, SEARCH_SET_PARTIAL_MATCH,
   SEARCH_SET_DATE_CREATED, SEARCH_SET_CROSS_REFERENCE_RESULTS,
-  SEARCH_SET_MOD_PREFERENCES_LOADED, SEARCH_SET_APPLY_TO_SINGLE_TAG, SEARCH_SET_READY_TO_FACET_SEARCH
+  SEARCH_SET_MOD_PREFERENCES_LOADED, SEARCH_SET_APPLY_TO_SINGLE_TAG,
+  SEARCH_SET_READY_TO_FACET_SEARCH, SEARCH_REMOVE_DATE_PUBMED_ADDED,
+  SEARCH_REMOVE_DATE_PUBMED_MODIFIED, SEARCH_REMOVE_DATE_PUBLISHED,
+  SEARCH_REMOVE_DATE_CREATED
 } from '../actions/searchActions';
 
 import _ from "lodash";
@@ -265,6 +268,47 @@ export default function(state = initialState, action) {
         ...state,
         applyToSingleTag: action.payload
       }
+
+        case 'SEARCH_SET_DATE_PUBMED_ADDED':
+            return {
+                ...state,
+                datePubmedAdded: action.payload
+            };
+        case 'SEARCH_REMOVE_DATE_PUBMED_ADDED':
+            return {
+                ...state,
+                datePubmedAdded: ''
+            };
+        case 'SEARCH_SET_DATE_PUBMED_MODIFIED':
+            return {
+                ...state,
+                datePubmedModified: action.payload
+            };
+        case 'SEARCH_REMOVE_DATE_PUBMED_MODIFIED':
+            return {
+                ...state,
+                datePubmedModified: ''
+            };
+        case 'SEARCH_SET_DATE_PUBLISHED':
+            return {
+                ...state,
+                datePublished: action.payload
+            };
+        case 'SEARCH_REMOVE_DATE_PUBLISHED':
+            return {
+                ...state,
+                datePublished: ''
+            };
+        case 'SEARCH_SET_DATE_CREATED':
+            return {
+                ...state,
+                dateCreated: action.payload
+            };
+        case 'SEARCH_REMOVE_DATE_CREATED':
+            return {
+                ...state,
+                dateCreated: ''
+            };
 
 //     case 'FETCH_POSTS':
 //       console.log('in postReducer case FETCH_POSTS');
