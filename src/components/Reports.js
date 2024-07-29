2d4cfe517cf45e561a91b8aaf00c56abb21576ae
sweng66 <sweng@stanford.edu>
2024-07-29 11:19:22 -0700

    adding a new Reports tab for Workflow Statistics

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,5 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+import { Spinner } from 'react-bootstrap';
 
 const file_upload_process_atp_id = "ATP:0000140";
 const col_width = '70px';
@@ -8,30 +12,43 @@ const WorkflowStatTable = () => {
   const [data, setData] = useState([]);
   const [mods, setMods] = useState([]);
   const [totals, setTotals] = useState({});
+  const [isLoadingData, setIsLoadingData] = useState(false);
+  const [key, setKey] = useState(0); // Add a key state to force re-rendering
+
+  const gridRef = useRef();
 
   useEffect(() => {
-    const url = process.env.REACT_APP_RESTAPI + "/workflow_tag/counters/?workflow_process_atp_id=" + file_upload_process_atp_id;
-    axios.get(url)
-      .then(response => {
-        const result = response.data;
-        setData(result);
+    const fetchData = async () => {
+      const url = process.env.REACT_APP_RESTAPI + "/workflow_tag/counters/?workflow_process_atp_id=" + file_upload_process_atp_id;
+      setIsLoadingData(true);
+      try {
+        const result = await axios.get(url);
+        setData(result.data);
 
-        const modsSet = new Set(result.map(item => item.mod_abbreviation));
+        const modsSet = new Set(result.data.map(item => item.mod_abbreviation));
         const modsArray = Array.from(modsSet);
         setMods(modsArray);
 
         const totalsObj = {};
-        result.forEach(item => {
+        result.data.forEach(item => {
           if (!totalsObj[item.wornflow_tag_name]) {
             totalsObj[item.wornflow_tag_name] = 0;
           }
           totalsObj[item.wornflow_tag_name] += item.tag_count;
         });
         setTotals(totalsObj);
-      })
-      .catch(error => {
+
+        // Update key to force re-render
+        setKey(prevKey => prevKey + 1);
+
+      } catch (error) {
         console.error('Error fetching data:', error);
-      });
+      } finally {
+        setIsLoadingData(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const getTagCount = (tagName, mod) => {
@@ -65,37 +82,61 @@ const WorkflowStatTable = () => {
   };
 
   const formatTagName = (tagName) => {
-    return tagName.replace(/^files?\s/, '');
+    return tagName.replace(/^files?\s/, '').replace('upload in progress', 'in progress');
   };
-    
+
+  const BoldRenderer = (props) => {
+    return <strong>{props.value}</strong>;
+  };
+
+  const columns = [
+    { 
+      headerName: '', 
+      field: 'tag_name', 
+      width: 300,  // Increased width for the first column
+      cellStyle: { textAlign: 'left', whiteSpace: 'normal' }, 
+      cellRendererFramework: BoldRenderer 
+    },
+    { headerName: 'Total', field: 'total', width: 150, cellStyle: { textAlign: 'left' } },
+    ...mods.map(mod => ({ headerName: mod, field: mod, width: 150, cellStyle: { textAlign: 'left' } })),
+  ];
+
+  const rowData = tagNames.map(tagName => {
+    const row = { 
+      tag_name: formatTagName(tagName), 
+      total: totals[tagName] || 0 
+    };
+    mods.forEach(mod => {
+      row[mod] = getTagCount(tagName, mod);
+    });
+    return row;
+  });
+
   return (
     <div>
       <h3>Workflow Statistics</h3>
-	<strong style={{ display: 'block', marginTop: '40px' }}>File Upload Current Status</strong>
-      <div style={containerStyle}>	
-        <table style={tableStyle}>
-          <thead>
-            <tr>
-              <th style={thStyle}></th>
-              <th style={thStyle}>Total</th>
-              {mods.map(mod => (
-                <th key={mod} style={thStyle}>{mod}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {tagNames.map(tagName => (
-              <tr key={tagName}>
-                <td style={thTdStyle}>{formatTagName(tagName)}</td>
-                <td style={thTdStyle}>{totals[tagName] || 0}</td>
-                {mods.map(mod => (
-                  <td key={`${tagName}-${mod}`} style={thTdStyle}>{getTagCount(tagName, mod)}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <strong style={{ display: 'block', marginTop: '40px' }}>File Upload Current Status</strong>
+      {isLoadingData ? (
+        <div className="text-center">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <div style={containerStyle}>
+          <div className="ag-theme-quartz" style={{ height: 500, width: '90%' }}>
+            <AgGridReact
+              key={key} // Add key prop to force re-render
+              ref={gridRef}
+              rowData={rowData}
+              columnDefs={columns}
+              pagination={true}
+              paginationPageSize={25}
+              domLayout="autoHeight"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
