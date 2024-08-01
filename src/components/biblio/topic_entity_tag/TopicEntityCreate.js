ac4135e581b16e0dfe78db0df84b951099395ccc
sweng66 <sweng@stanford.edu>
2024-08-01 13:36:59 -0700

    updated to use old version of PulldownMenu for the moment

--- src/components/biblio/topic_entity_tag/TopicEntityCreate.js
+++ src/components/biblio/topic_entity_tag/TopicEntityCreate.js
@@ -19,7 +19,7 @@ import {
   getCurieToNameTaxon,
   getModToTaxon
 } from "./TaxonUtils";
-import {PulldownMenu} from "../PulldownMenu";
+import {PulldownMenu} from "../PulldownMenuOld";
 import {FetchTypeaheadOptions} from "../FetchTypeahead";
 import Container from "react-bootstrap/Container";
 import ModalGeneric from "../ModalGeneric";
