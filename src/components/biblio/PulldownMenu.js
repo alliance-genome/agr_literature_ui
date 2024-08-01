313f6e98fb2ce9491e3f39fb202488575de26c7b
sweng66 <sweng@stanford.edu>
2024-08-01 11:42:09 -0700

    fixed PulldownMenu.js to work for multi rows

--- src/components/biblio/PulldownMenu.js
+++ src/components/biblio/PulldownMenu.js
@@ -1,22 +1,31 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import {changeFieldEntityAddGeneralField} from "../../actions/biblioActions";
-import Form from "react-bootstrap/Form"
+import PropTypes from 'prop-types';
+import Form from "react-bootstrap/Form";
 
-export const PulldownMenu = ({id, value, pdList, optionToName}) => {
-  const dispatch = useDispatch();
-  return (<div>                                                                                                                         
-    <Form.Control
-      as="select"
-      id={id}
-      type={id}
-      value={value}
-      onChange={(e) => { dispatch(changeFieldEntityAddGeneralField(e)) } } >                                                            
-      { pdList.map((optionValue, index) => (
-        <option key={`{id} ${optionValue}`}
-          value={optionValue}>{optionToName[optionValue]}                                                                                
-        </option>
-      ))}                                                                                                                               
-    </Form.Control>                                                                                                                     
-  </div>);
-}
+export const PulldownMenu = ({ id, value, pdList, optionToName, onChange }) => {
+  return (
+    <div>
+      <Form.Control
+        as="select"
+        id={id}
+        type={id}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {pdList.map((optionValue, index) => (
+          <option key={`{id} ${optionValue}`} value={optionValue}>
+            {optionToName[optionValue]}
+          </option>
+        ))}
+      </Form.Control>
+    </div>
+  );
+};
+
+PulldownMenu.propTypes = {
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  pdList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  optionToName: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
