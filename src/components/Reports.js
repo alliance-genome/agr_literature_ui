5be6d3cbadf7f8c5ab49799a77be1222289fb59a
Juancarlos <azurebrd@its.caltech.edu>
2024-12-06 10:06:25 +0000

    ReportsDatePicker fixed some warnings, but not sure that the offset it working properly.  It looks good, but bears testing at different times

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { Spinner, Tabs, Tab } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Spinner, Tabs, Tab, Button, ButtonGroup } from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-import { DatePicker } from './search/Facets';
+import Form from 'react-bootstrap/Form';
 
 import { setDateFileUpload } from '../actions/reportsActions';
 
@@ -13,6 +12,10 @@ import axios from 'axios';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
+
+import DateRangePicker from '@wojtekmaj/react-daterange-picker'
+import '@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css';
+import 'react-calendar/dist/Calendar.css';
 
 const WorkflowStatTable = ({ workflowProcessAtpId, title, tagNames, nameMapping }) => {
   const [data, setData] = useState([]);
@@ -117,10 +120,10 @@ const WorkflowStatTable = ({ workflowProcessAtpId, title, tagNames, nameMapping 
         </div>
       ) : (
         <div style={containerStyle}>
-          <Container style={{ width: '90%' }}>
+          <Container fluid style={{ width: '90%' }}>
             <Row>
               <Col>
-                <DatePicker facetName='Date Range' currentValue={dateFileUpload} setValueFunction={setDateFileUpload} />
+                <ReportsDatePicker facetName={workflowProcessAtpId} currentValue={dateFileUpload} setValueFunction={setDateFileUpload} />
               </Col>
             </Row>
             <Row>
@@ -144,6 +147,115 @@ const WorkflowStatTable = ({ workflowProcessAtpId, title, tagNames, nameMapping 
     </div>
   );
 };
+
+const ReportsDatePicker = ({ facetName, currentValue, setValueFunction }) => {
+    const dispatch = useDispatch();
+
+    function formatDateRange(dateRange){
+            let dateStart=dateRange[0].getFullYear()+"-"+parseInt(dateRange[0].getMonth()+1).toString().padStart(2,'0')+"-"+dateRange[0].getDate().toString().padStart(2,'0');
+            let dateEnd=dateRange[1].getFullYear()+"-"+parseInt(dateRange[1].getMonth()+1).toString().padStart(2,'0')+"-"+dateRange[1].getDate().toString().padStart(2,'0');
+            return [dateStart,dateEnd];
+    }
+
+    function formatToUTCString(dateRange) {
+        if (dateRange !== '') {
+            let dateStart = new Date (dateRange[0]);
+            let dateEnd = new Date(dateRange[1]);
+            let offset = dateStart.getTimezoneOffset();
+            dateStart.setMinutes(dateStart.getMinutes() + offset);
+            dateEnd.setMinutes(dateEnd.getMinutes() + offset);
+            return [dateStart, dateEnd]; 
+//             let parsedDateStart = Date.parse(dateRange[0]) + (offset * 60000);
+//             let parsedDateEnd = Date.parse(dateRange[1]) + (offset * 60000);
+// console.log('parsedDateStart');
+// console.log( parsedDateStart );
+// console.log('parsedDateEnd');
+// console.log( parsedDateEnd );
+//             return [new Date("2017-01-01"), new Date("2017-01-01")]
+//             return [parsedDateStart, parsedDateEnd];
+        } else {
+// console.log('formatToUTCString blank string');
+            return "";
+        }
+    }
+
+// console.log('currentValue');
+// console.log(currentValue);
+// console.log('formatToUTCString(currentValue)');
+// const blah = formatToUTCString(currentValue)
+// console.log(blah);
+// console.log('blah');
+
+// [ 1483257600000, 1483257600000 ]
+// [ "2017-01-01", "2017-01-01" ]
+//  const bleh = [ 1483257600000, 1483257600000 ]
+//  const bleh = [new Date(), new Date()]
+//  const bleh = [new Date("2017-01-01"), new Date("2017-01-01")]
+//  const bleh = [ "2017-01-01", "2017-01-01" ]
+
+    function handleFixedTimeClick(timeframe) {
+        var today = new Date();
+        let newDate = ['',''];
+        if (timeframe === 'Day') {
+            newDate = formatDateRange([today,today]);
+        }
+        else if (timeframe === 'Week') {
+            let lastWeek = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+            newDate = formatDateRange([lastWeek,today]);
+        }
+        else if (timeframe === 'Month') {
+            let lastMonth = new Date(today.getTime() - 30 * 24 * 60 * 60 * 1000);
+            newDate = formatDateRange([lastMonth,today]);
+        }
+        else if (timeframe === 'Year') {
+            let lastYear = new Date(today.getTime() - 365 * 24 * 60 * 60 * 1000);
+            newDate = formatDateRange([lastYear,today]);
+        }
+        dispatch(setValueFunction(newDate));
+//         dispatch(searchReferences());
+    }
+
+    function handleDateChange(newDateRangeArr){
+        if (newDateRangeArr === null) {
+            dispatch(setValueFunction(''));
+//             dispatch(setSearchResultsPage(1));
+//             dispatch(searchReferences());
+        }
+        else if(!isNaN(Date.parse(newDateRangeArr[0])) && !isNaN(Date.parse(newDateRangeArr[1]))){
+            dispatch(setValueFunction(formatDateRange(newDateRangeArr)));
+//             dispatch(setSearchResultsPage(1));
+//             dispatch(searchReferences());
+        }
+    }
+
+//                   <Form.Control as='select' id='license' name='license' style={{width: "10em"}} value={newLicense} onChange={(e) => setNewLicense(e.target.value)} >
+  const dateOptions = [ 'Date added to ABC', 'Date Published' ];
+  const newLicense = 'blah';
+  const licenseName = 'bleh';
+  const licenseToShow = 'bleh';
+
+//             <h5>{facetName}</h5>
+    return(
+            <div key={facetName} style={{ display: 'flex', alignItems: 'center', textAlign: "left", paddingLeft: "2em", paddingBottom: "0.5em" }}>
+              <Form.Control as='select' id='license' name='license' style={{ width: "12em", marginRight: "3em" }} value={newLicense} >
+                {dateOptions.map((optionValue, index) => (
+                    <option value={optionValue} defaultValue={licenseToShow !== '' ? licenseName : null} key={index}>{optionValue}</option>
+                ))}
+              </Form.Control>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <ButtonGroup aria-label="DateSetter" size ="sm" style={{display: "block"}}>
+                  <Button variant="secondary" style={{'borderBottomLeftRadius' : 0}} onClick= { () => { handleFixedTimeClick('Day') } } >Day</Button>
+                  <Button variant="secondary" onClick={ () => { handleFixedTimeClick('Week') } } >Week</Button>
+                  <Button variant="secondary" onClick={ () => { handleFixedTimeClick('Month') } } >Month</Button>
+                  <Button variant="secondary" style={{'borderBottomRightRadius' : 0}} onClick= { () => { handleFixedTimeClick('Year') } } >Year</Button>
+                </ButtonGroup>
+                <DateRangePicker value={formatToUTCString(currentValue)} onChange= { (newDateRangeArr) => { handleDateChange(newDateRangeArr) } } />
+              </div>
+            </div>
+    )
+} // const ReportsDatePicker = ({ facetName, currentValue, setValueFunction })
+
+//             <DateRangePicker value={bleh} onChange= { (newDateRangeArr) => { handleDateChange(newDateRangeArr) } } />
 
 const WorkflowStatTablesContainer = () => {
   const file_upload_name_mapping = {
