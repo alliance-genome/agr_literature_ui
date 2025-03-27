e50accda5f850a6bd65ca1064829994a0959938a
pjhale <Paul.Hale@jax.org>
2025-03-27 15:43:14 -0600

    Adding Cache-Buster
    Adding the cache buster.  This looks like it will reload the page every single time.  If this is not what we want we seem to be able to assign a version in a build step and check then.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 // import logo from './logo.svg';
 import React from 'react';
+import CacheBuster from 'react-cache-buster';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppWithRouterAccess from './components/AppWithRouterAccess';
 
 const App = () => (
-    <Router>
-      <AppWithRouterAccess/>
-    </Router>
+    <CacheBuster
+        currentVersion={Math.random().toString(36).substring(2, 8)}
+        isEnabled={process.env.NODE_ENV === "production"} //If false, the library is disabled.
+        isVerboseMode={false} //If true, the library writes verbose logs to console.
+        loadingComponent={null} //If not pass, nothing appears at the time of new version check.
+        metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
+        onCacheClear={() => window.location.reload(true)}
+    >
+        <Router>
+          <AppWithRouterAccess/>
+        </Router>
+    </CacheBuster>
 );
 export default App;
