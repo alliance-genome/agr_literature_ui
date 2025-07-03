7dbf4ce7c9da06b2f5ef206db5b65f183f836e98
Juancarlos <azurebrd@its.caltech.edu>
2025-07-03 20:03:28 +0000

    Additional message for curators to change browser settings to avoid automated okta logout

--- src/components/NotLoggedInBar.js
+++ src/components/NotLoggedInBar.js
@@ -12,8 +12,8 @@ const NotLoggedInBar = () => {
   const everSignedIn = useSelector(state => state.isLogged.everSignedIn);
   const loggedClass = (everSignedIn) ? 'NotLoggedInAlert-everLogged' : '';
   return (
-    (!isSignedIn) && 
-    <Alert className={`fade alert alert-danger show ${loggedClass}`} variant='danger'><FontAwesomeIcon icon={faExclamationTriangle} /> You are not signed in. Please sign in to edit, tag, or download PDFs. <FontAwesomeIcon icon={faExclamationTriangle} /></Alert>
+    (!isSignedIn) &&
+    <Alert className={`fade alert alert-danger show ${loggedClass}`} variant='danger'><FontAwesomeIcon icon={faExclamationTriangle} /> You are not signed in. Please sign in to edit, tag, or download PDFs. <FontAwesomeIcon icon={faExclamationTriangle} /><br/>Turn off cross site tracking protection and allow third party cookies to avoid <a href="https://agr-jira.atlassian.net/wiki/spaces/LITERATURE/pages/1122140162/Unexpected+Logout" rel="noreferrer noopener" target="_blank">unexpected logout</a>.</Alert>
   )
 }
 
