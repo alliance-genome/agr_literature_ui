d88940333af4df8a7a9928c0a5c3b954f4024c3f
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2025-12-17 18:52:59 -0800

    fix(NotLoggedInBar): do not cover content with the alert bar

--- src/components/NotLoggedInBar.js
+++ src/components/NotLoggedInBar.js
@@ -9,11 +9,9 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
 const NotLoggedInBar = () => {
   const isSignedIn = useSelector(state => state.isLogged.isSignedIn);
-  const everSignedIn = useSelector(state => state.isLogged.everSignedIn);
-  const loggedClass = (everSignedIn) ? 'NotLoggedInAlert-everLogged' : '';
   return (
     (!isSignedIn) &&
-    <Alert className={`fade alert alert-danger show ${loggedClass}`} variant='danger'><FontAwesomeIcon icon={faExclamationTriangle} /> You are not signed in. Please sign in to edit, tag, or download PDFs. <FontAwesomeIcon icon={faExclamationTriangle} /><br/>Turn off cross site tracking protection and allow third party cookies to avoid <a href="https://agr-jira.atlassian.net/wiki/spaces/LITERATURE/pages/1122140162/Unexpected+Logout" rel="noreferrer noopener" target="_blank">unexpected logout</a>.</Alert>
+    <Alert className="fade alert alert-danger show NotLoggedInAlert-everLogged" variant='danger'><FontAwesomeIcon icon={faExclamationTriangle} /> You are not signed in. Please sign in to edit, tag, or download PDFs. <FontAwesomeIcon icon={faExclamationTriangle} /><br/>Turn off cross site tracking protection and allow third party cookies to avoid <a href="https://agr-jira.atlassian.net/wiki/spaces/LITERATURE/pages/1122140162/Unexpected+Logout" rel="noreferrer noopener" target="_blank">unexpected logout</a>.</Alert>
   )
 }
 
